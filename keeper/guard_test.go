cb366abc132f4a2ab137e66bec78a4461906848d
LiYuqing <im.yuqlee@gmail.com>
2022-07-13 16:20:51 +0800

    Sign with assignee

--- keeper/guard_test.go
+++ keeper/guard_test.go
@@ -48,9 +48,20 @@ func TestGuard(t *testing.T) {
 		assert.Nil(err)
 	}
 
-	// invalid nonce
+	// data should be base64 RawURLEncoding and none blank
 	signature, data := makeTestRequest(user, node, ephmr, nil, 1024, grace)
-	res, err := Guard(bs, signer, identity, signature, data)
+	res, err := Guard(bs, signer, identity, signature, "")
+	assert.NotNil(err)
+
+	// identity is not equal
+	signature, data = makeTestRequestWithInvalidIdentity(user, node, ephmr, nil, 1039, grace, "", "", "")
+	res, err = Guard(bs, signer, identity, signature, data)
+	assert.NotNil(err)
+	assert.Contains(err.Error(), "invalid idenity ")
+
+	// invalid nonce
+	signature, data = makeTestRequest(user, node, ephmr, nil, 1024, grace)
+	res, err = Guard(bs, signer, identity, signature, data)
 	assert.Nil(res)
 	assert.Nil(err)
 	key := crypto.PublicKeyBytes(crypto.PublicKey(user))
@@ -112,6 +123,31 @@ func TestGuard(t *testing.T) {
 	available, err = bs.CheckLimit(lkey, SecretLimitWindow, SecretLimitQuota, false)
 	assert.Equal(SecretLimitQuota-5, available)
 	assert.Nil(err)
+
+	// invalid assignee
+	signature, data = makeTestRequestWithAssigneeAndRotation(user, node, ephmr, nil, 1039, grace, identity, "", "")
+	res, err = Guard(bs, signer, identity, signature, data)
+	assert.NotNil(err)
+	assert.Contains(err.Error(), "invalid assignee ")
+	// valid assignee
+	userPub, err := crypto.PubKeyFromBase58(identity)
+	assert.Nil(err)
+	assignee := crypto.PublicKeyBytes(userPub)
+	sig, err := crypto.Sign(user, assignee)
+	assert.Nil(err)
+	assignee = append(assignee, sig...)
+	signature, data = makeTestRequestWithAssigneeAndRotation(user, node, ephmr, nil, 1039, grace, hex.EncodeToString(assignee), "", "")
+	res, err = Guard(bs, signer, identity, signature, data)
+	assert.Nil(err)
+	watcherSeed := make([]byte, 32)
+	_, err = rand.Read(watcherSeed)
+	assert.Nil(err)
+	signature, data = makeTestRequestWithAssigneeAndRotation(user, node, ephmr, nil, 1040, grace, hex.EncodeToString(assignee), "", hex.EncodeToString(watcherSeed))
+	res, err = Guard(bs, signer, identity, signature, data)
+	assert.Nil(err)
+	assignee, err = bs.ReadAssignee(crypto.PublicKeyBytes(userPub))
+	assert.Nil(err)
+	assert.Len(assignee, 128)
 }
 
 func TestAssigneeAndRotation(t *testing.T) {
@@ -175,6 +211,42 @@ func makeTestRequestWithAssigneeAndRotation(user kyber.Scalar, signer kyber.Poin
 	msg = append(msg, buf...)
 	data := map[string]interface{}{
 		"identity":  crypto.PublicKeyString(pkey),
+		"ephemeral": hex.EncodeToString(ephmr),
+		"nonce":     nonce,
+		"grace":     grace,
+		"watcher":   watcher,
+	}
+	if rtt != nil {
+		msg = append(msg, rtt[:]...)
+		data["rotate"] = hex.EncodeToString(rtt)
+	}
+	if assignee != "" {
+		userPub := crypto.PublicKeyBytes(crypto.PublicKey(user))
+		msg = append(msg, userPub...)
+		sig, _ := crypto.Sign(user, userPub)
+		msg = append(msg, sig...)
+		data["assignee"] = assignee
+	}
+	b, _ := json.Marshal(data)
+	cipher := crypto.Encrypt(signer, user, b)
+	sig, _ := crypto.Sign(user, msg)
+	return hex.EncodeToString(sig), base64.RawURLEncoding.EncodeToString(cipher[:])
+}
+
+func makeTestRequestWithInvalidIdentity(user kyber.Scalar, signer kyber.Point, ephmr, rtt []byte, nonce, grace uint64, assignee, rotation, watcher string) (string, string) {
+	pkey := crypto.PublicKey(user)
+	msg := crypto.PublicKeyBytes(pkey)
+	msg = append(msg, ephmr...)
+	buf := make([]byte, 8)
+	binary.BigEndian.PutUint64(buf, nonce)
+	msg = append(msg, buf...)
+	binary.BigEndian.PutUint64(buf, grace)
+	msg = append(msg, buf...)
+	suite := bn256.NewSuiteBn256()
+	intruder := suite.Scalar().Pick(random.New())
+	intruderPub := crypto.PublicKey(intruder)
+	data := map[string]interface{}{
+		"identity":  crypto.PublicKeyString(intruderPub),
 		"ephemeral": hex.EncodeToString(ephmr),
 		"nonce":     nonce,
 		"grace":     grace,
